eedba2b80d04810d512da34c5d3836cc34e831fb
Roberto Mardeni <rmardeni@gmail.com>
2020-09-21 10:20:18 -0400

    Added PS functions for testing

---
+++ src/functionApp/testFrontDoor/run.ps1
@@ -0,0 +1,24 @@
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+# Write to the Azure Functions log stream.
+Write-Host "PowerShell HTTP trigger function processed a request."
+
+$uri = "http://customer-demo.azurefd.net/"
+$statusCode = [HttpStatusCode]::OK
+$body = "It looks good!"
+
+Try {
+    Invoke-WebRequest -Uri $uri -ErrorAction Stop
+} Catch {
+    $body = "Request Failed :("
+    $statusCode = [HttpStatusCode]::InternalServerError
+}
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+    StatusCode = $statusCode
+    Body = $body
+})
