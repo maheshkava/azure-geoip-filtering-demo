99bf79950ef1e33b03ee8ddc38c90b0e03d77340
Roberto Mardeni <rmardeni@gmail.com>
2020-09-21 21:18:26 -0400

    Added commands to retrieve keys for tests

--- src/runTests.sh
+++ src/runTests.sh
@@ -1,3 +1,7 @@
+# Retrieve keys for tests
+code1=$(az functionapp keys list -n customer-demo-client-us -g customer-demo --query masterKey -o tsv)
+code2=$(az functionapp keys list -n customer-demo-client-germany -g customer-demo --query masterKey -o tsv)
+# Run tests
 printf "Running Tests from US"
 curl -d "" -X POST "https://customer-demo-client-us.azurewebsites.net/api/testAppGateway?code=$code1"
 printf "\n"
